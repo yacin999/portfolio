bb3ea1df58e9a16fcf9895c760c52f2970fb3046
yacine <yacinomar999@gmail.com>
2024-06-03 20:12:20 +0100

    add hero title

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Button } from './ui/button'
 
-type Props = {}
 
-const Hero = (props: Props) => {
+const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='w-screen'>
+      <h1 className="text-4xl font-bold mb-4">Hi, I'm Kelalech Omar</h1>
+      <p className="text-xl mb-8">A passionate Web Developer specializing in modern web technologies.</p>
+      <div>
+        <Button>
+          Hire me
+        </Button>
+      </div>
+    </section>
   )
 }
 
