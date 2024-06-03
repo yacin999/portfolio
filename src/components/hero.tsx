6c85cf401f0a7c18607f2bb68aaf7dc0b46c1477
yacine <yacinomar999@gmail.com>
2024-06-03 20:14:06 +0100

    add styling to hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,12 +5,17 @@ import { Button } from './ui/button'
 const Hero = () => {
   return (
     <section className='w-screen'>
-      <h1 className="text-4xl font-bold mb-4">Hi, I'm Kelalech Omar</h1>
-      <p className="text-xl mb-8">A passionate Web Developer specializing in modern web technologies.</p>
+      <div className='flex flex-col sm:flex-row'>
+        <h1 className="text-4xl font-bold mb-4">Hi, I'm Kelalech Omar</h1>
+        <p className="text-xl mb-8">A passionate Web Developer specializing in modern web technologies.</p>
+        <div>
+          <Button>
+            Hire me
+          </Button>
+        </div>
+      </div>
       <div>
-        <Button>
-          Hire me
-        </Button>
+
       </div>
     </section>
   )
