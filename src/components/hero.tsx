087960969053a56bd455e0dd57760a3e1bc2b053
yacine <yacinomar999@gmail.com>
2024-06-05 22:00:47 +0100

    update hero bg

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,7 +5,7 @@ import ComputerCanvas from './computer-canvas'
 
 const Hero = () => {
   return (
-    <section className='dark:bg-slate-950 w-full h-[calc(100vh-70px)] bg-hero bg-cover flex items-center mt-[70px]'>
+    <section className=' w-full h-[calc(100vh-70px)] bg-hero bg-cover flex items-center mt-[70px]'>
       <div className='w-full h-full flex flex-col sm:flex-row sm:items-center sm:justify-between '>
         <div className='flex w-full flex-col items-start justify-start  px-10 '>
           <h1 className="flex items-center gap-2 text-4xl font-bold mb-4">
