b5d0846cd21ac302fb30b537808443b77112f9e4
yacine <yacinomar999@gmail.com>
2024-06-12 21:50:15 +0100

    update contact form

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -36,23 +36,24 @@ export function Form() {
           <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
+          <Label htmlFor="phone">Phone Number</Label>
+          <Input id="phone" placeholder="07........" type="text"/>
         </LabelInputContainer>
         <LabelInputContainer className="mb-8">
-          <Label htmlFor="twitterpassword">Your twitter password</Label>
+          <Label htmlFor="subject">Subject</Label>
           <Input
-            id="twitterpassword"
-            placeholder="••••••••"
-            type="twitterpassword"
+            id="ubject"
+            placeholder="Subject"
+            type="text"
           />
         </LabelInputContainer>
+        
 
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Sign up &rarr;
+          Send &rarr;
           <BottomGradient />
         </button>
       </form>
