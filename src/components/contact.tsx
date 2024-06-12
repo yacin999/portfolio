31d3ca903e8af6b3823e4acdf0a61a339072206d
yacine <yacinomar999@gmail.com>
2024-06-12 21:44:04 +0100

    add form to contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Form } from './ui/form'
 
 type Props = {}
 
@@ -6,10 +7,11 @@ const Contact = (props: Props) => {
   return (
     <section className='flex'>
       <div>
-
+        Email : yacinomar999@gamil.com
+        phone : 0793292628
       </div>
       <div>
-        
+        <Form/>
       </div>
     </section>
   )
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -8,7 +8,7 @@ import { ArrowUpRight } from 'lucide-react'
 
 const Projects = () => {
   return (
-    <section className='relative w-full'>
+    <section className='relative w-full mb-40'>
       {/* start square gradiant grid background */}
         <div className="absolute z-10 bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]">
         </div>
