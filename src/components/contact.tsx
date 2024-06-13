65dfaf08ce246c25dc4ad2cc9216ca2dae5d1f12
yacine <yacinomar999@gmail.com>
2024-06-13 21:23:42 +0100

    adjust submit form btn

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -6,11 +6,11 @@ type Props = {}
 const Contact = (props: Props) => {
   return (
     <section className='flex'>
-      <div>
-        Email : yacinomar999@gamil.com
-        phone : 0793292628
+      <div className='flex-1'>
+        <div>Email : yacinomar999@gamil.com</div>
+        <div>phone : 0793292628</div>
       </div>
-      <div>
+      <div className='flex-1'>
         <Form/>
       </div>
     </section>
--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -4,6 +4,7 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { cn } from "@/lib/utils";
 import { Textarea } from "./textarea";
+import { SendHorizonal } from "lucide-react";
 
 
 export function Form() {
@@ -58,10 +59,11 @@ export function Form() {
         
 
         <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          className="flex items-center justify-center gap-2 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600  dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Send &rarr;
+          Send 
+          <SendHorizonal className="relative" size={15}/>  
           <BottomGradient />
         </button>
       </form>
