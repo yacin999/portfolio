f8a62c105d0f35970311bd1b50709d3b5004386b
yacine <yacinomar999@gmail.com>
2024-06-14 11:40:06 +0100

    update contact design

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -12,17 +12,17 @@ const Contact = (props: Props) => {
   return (
     <section className='relative flex items-center px-10'>
       <GradiantGridBackground/>
-      <div className='relative z-30 flex-1'>
+      <div className='relative z-30 flex-1 flex flex-col items-center'>
         <div className='mb-10'>
-          <div>Email : boutiqueserviceinfo.com</div>
-          <div>phone : 0676299345</div>
+          <p className='text-md text-blue-500 border-2 border-blue-500 rounded-full bg-background px-5 py-2 w-fit mb-4'>Email : boutiqueserviceinfo.com</p>
+          <div className='text-md  text-blue-500 border-2 border-blue-500 rounded-full bg-background  px-5 py-2 w-fit'>phone : 0676299345</div>
         </div>
-        <div className='flex gap-3 items-center'>
+        <div className='flex gap-5 items-center'>
           {socialMedia.map(icon=>(
             <Image  
               src={icon.src}
-              width={25}
-              height={25}
+              width={40}
+              height={40}
               alt={icon.name}
               className='cursor-pointer'
             />
