610bdf8df73864955aef9d7b87ff6abcfada7e22
yacine <yacinomar999@gmail.com>
2024-06-26 09:53:25 +0100

    update contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -27,19 +27,22 @@ const Contact = () => {
         transition={{duration : 0.4, delay : 0.4}}
         viewport={{once : true}}
       >
-        <div className='mb-20'>
-          <p className='text-md text-[#00CC00] border-2 border-[#00CC00] rounded-full bg-background px-5 py-2 w-fit mb-4'>{t("contact.email")} : boutiqueserviceinfo.com</p>
-          <div className='text-md  text-[#00CC00] border-2 border-[#00CC00] rounded-full bg-background  px-5 py-2 w-fit'>{t("contact.phone")} : 0676299345</div>
-        </div>
-        <div className='flex gap-5 items-center'>
-          {socialMedia.map(icon=> {
-            const Component = iconsComponents[icon.component]
-            return <Link key={icon.name} href={icon.link}>
-              <Component size={30} className="fill-[#00CC00]"/>
-            </Link>
-          })}
+        <div className='flex flex-col items-center'>
+          <div className='mb-20 flex flex-col items-center'>
+            <p className='text-md text-[#00CC00] border-2 border-[#00CC00] rounded-full bg-background px-5 py-2 w-fit mb-4'>{t("contact.email")} : boutiqueserviceinfo.com</p>
+            <div className='text-md  text-[#00CC00] border-2 border-[#00CC00] rounded-full bg-background  px-5 py-2 w-fit'>{t("contact.phone")} : 0676299345</div>
+          </div>
+          <div className='flex gap-5'>
+            {socialMedia.map(icon=> {
+              const Component = iconsComponents[icon.component]
+              return <Link key={icon.name} href={icon.link}>
+                <Component size={30} className="fill-[#00CC00]"/>
+              </Link>
+            })}
+          </div>
         </div>
       </motion.div>
+      
       <motion.div 
         className='flex-1'
         initial={{ x : 40, opacity: 0 }}
