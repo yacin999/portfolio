4715e8f94ade581c4dff66d4397971626d50b68e
yacine <yacinomar999@gmail.com>
2024-07-09 21:40:18 +0100

    install next bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    transpilePackages: ['three'],
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//     enabled: process.env.ANALYZE === 'true',
+// })
+   
+
+
+//   /** @type {import('next').NextConfig} */
+//   const nextConfig = {
+//     transpilePackages: ['three'],
+//   }
+   
+//   module.exports = withBundleAnalyzer(nextConfig)
+
+
+
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const config = {
+  enabled: process.env.ANALYZE === 'true',
 };
 
-export default nextConfig;
+const nextConfig = {
+  transpilePackages: ['three'],
+};
+
+export default withBundleAnalyzer(config)(nextConfig);
