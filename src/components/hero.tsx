47e23327568b577a1e61d7ecbadee3db04e40c08
yacine <yacinomar999@gmail.com>
2024-07-12 20:23:31 +0100

    update hero scene style

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -120,12 +120,14 @@ const Hero = () => {
 
       <Scene
         style={{
-          position : "absolute",
-          top : 0,
-          left : 0,
+          // position : "absolute",
+          // top : 0,
+          // left : 0,
           // width: '100vw',
           // height: '100vh',
+          display : "block",
           width : '100%',
+          height : '100%',
           pointerEvents: 'none',
         }}
         eventSource={ref}
