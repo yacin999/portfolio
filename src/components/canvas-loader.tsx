f34f0f7ff972936c9b405187fc830629aedeb3f2
yacine <yacinomar999@gmail.com>
2024-08-05 12:48:33 +0100

    update canvas loader

--- src/components/canvas-loader.tsx
+++ src/components/canvas-loader.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
-import { useProgress, Html } from '@react-three/drei'
-
 
 const CanvasLoader = () => {
-  // const { progress } = useProgress()
   return (
-    <Html>
-      {/* <span className='canvas-loader'></span>
-      <p className='text-sm text-[#F1F1F1] font-extrabold'>{progress.toFixed(2)}%</p> */}
       <svg 
         className="h-[37px] w-[37px] overflow-visible origin-center"
         x="0px" 
@@ -33,7 +27,6 @@ const CanvasLoader = () => {
             strokeLinecap="round"
             d="M36.63 31.746 c0 -13.394 -7.3260000000000005 -25.16 -18.13 -31.375999999999998 C7.696 6.66 0.37 18.352 0.37 31.746 c5.328 3.108 11.544 4.8839999999999995 18.13 4.8839999999999995 S31.301999999999996 34.854 36.63 31.746z"></path>
         </svg>
-    </Html>
   )
 }
 
